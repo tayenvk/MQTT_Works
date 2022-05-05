20464ccbb5c263950d876815e2325efbbfb34290
tayen.vankeer <tayen.vankeer@student.kuleuven.be>
2022-05-05 12:41:38 +0200

    quick save

--- main/main.c
+++ main/main.c
@@ -5,18 +5,13 @@
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_netif.h"
-
 #include "freertos/semphr.h"
 #include "freertos/queue.h"
-
 #include "lwip/sockets.h"
 #include "lwip/dns.h"
 #include "lwip/netdb.h"
-
 #include "esp_log.h"
 #include "mqtt_client.h"
-
-///////////////////////////////////////////////////////////////////////////////////////////////
 #include <stdlib.h>
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
@@ -36,10 +31,9 @@
 #include "driver/uart.h"
 #include "esp_timer.h"
 
-//pump input
+//ADC Attenuation
+#define ADC_EXAMPLE_ATTEN           ADC_ATTEN_DB_11
 #define pump_gpio (21)
-static uint8_t pump_in_state = 0;
-
 //ADC Channels
 #if CONFIG_IDF_TARGET_ESP32
 #define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_6
@@ -57,9 +51,6 @@ static const char *TAG_CH[5][10] = {{"ADC1_CH6"},{"ADC1_CH0"},{"ADC1_CH3"},{"ADC
 static const char *TAG_CH[2][10] = {{"ADC1_CH2"}, {"ADC1_CH0"},{"ADC1_CH3"},{"ADC1_CH4"},{"ADC2_CH0"}};
 #endif
 
-//ADC Attenuation
-#define ADC_EXAMPLE_ATTEN           ADC_ATTEN_DB_11
-
 //ADC Calibration
 #if CONFIG_IDF_TARGET_ESP32
 #define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_VREF
@@ -71,10 +62,19 @@ static const char *TAG_CH[2][10] = {{"ADC1_CH2"}, {"ADC1_CH0"},{"ADC1_CH3"},{"AD
 #define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_TP_FIT
 #endif
 
-static int adc_raw[5][10];
-static const char *TAG = "ADC SINGLE";
-esp_mqtt_client_handle_t client;
+#define BUTTON_GPIO_NUM_DEFAULT     0
+#define BUTTON_WAKEUP_LEVEL_DEFAULT     0
+#define SERVO_MIN_PULSEWIDTH_US (575) // Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH_US (2460) // Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE        (200)   // Maximum angle in degree upto which servo can rotate
+#define SERVO_PULSE_GPIO        (12)   // GPIO connects to the PWM signal line   
 
+static int adc_raw[5][10];
+static uint8_t pump_in_state = 0;
+static const char *TAG = "ADC SINGLE";
+static const char *TAG_servo = "example";
+static const char *TAGE = "MQTT_TCP";
+esp_mqtt_client_handle_t client;
 static esp_adc_cal_characteristics_t adc1_chars;
 static esp_adc_cal_characteristics_t adc1_1_chars;
 static esp_adc_cal_characteristics_t adc1_2_chars;
@@ -82,26 +82,6 @@ static esp_adc_cal_characteristics_t adc1_3_chars;
 static esp_adc_cal_characteristics_t adc2_chars;
 
 //////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-#define BUTTON_GPIO_NUM_DEFAULT     0
-#define BUTTON_WAKEUP_LEVEL_DEFAULT     0
-
-static const char *TAG_servo = "example";
-
-// You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
-#define SERVO_MIN_PULSEWIDTH_US (575) // Minimum pulse width in microsecond
-#define SERVO_MAX_PULSEWIDTH_US (2460) // Maximum pulse width in microsecond
-#define SERVO_MAX_DEGREE        (200)   // Maximum angle in degree upto which servo can rotate
-
-#define SERVO_PULSE_GPIO        (12)   // GPIO connects to the PWM signal line   
-
-
-static const char *TAGE = "MQTT_TCP";
-
 
 static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
@@ -137,8 +117,8 @@ void wifi_connection()
     esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
     wifi_config_t wifi_configuration = {
         .sta = {
-            .ssid = "telenet-5274953",
-            .password = "ezP8dcmvMvev"}};
+            .ssid = "ONEPLUS_6T_joachim",
+            .password = "ee5veryfun"}};
     esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
     // 3 - Wi-Fi Start Phase
     esp_wifi_start();
@@ -215,7 +195,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 static void mqtt_app_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
-        .uri = "mqtt://192.168.0.105",//here you have
+        .uri = "mqtt://192.168.157.92",//here you have
     };
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
@@ -262,7 +242,6 @@ void DHT_task(void *pvParameter)
 {
     setDHTgpio( 21 );
     printf( "Starting DHT Task\n\n");
-
     while(1) {
     
         printf("=== Reading DHT ===\n" );
@@ -274,9 +253,9 @@ void DHT_task(void *pvParameter)
         char hum[50];
         sprintf( temp,"%.1f\n", getHumidity() );
         sprintf(hum, "%.1f\n", getTemperature() );
-        esp_mqtt_client_publish(client,"ESP32/sensorID", "5", 0, 1, 0);
+        esp_mqtt_client_publish(client,"ESP32/sensorID", "4", 0, 1, 0);
         esp_mqtt_client_publish(client,"ESP32/value", temp, 0, 1, 0);
-        esp_mqtt_client_publish(client,"ESP32/sensorID", "6", 0, 1, 0);
+        esp_mqtt_client_publish(client,"ESP32/sensorID", "5", 0, 1, 0);
         esp_mqtt_client_publish(client,"ESP32/value", hum, 0, 1, 0);
         
         // -- wait at least 2 sec before reading again ------------
@@ -295,44 +274,14 @@ static inline uint32_t example_convert_servo_angle_to_duty_us(int angle)
 
 void app_main(void)
 {   
-    const int button_gpio_num = BUTTON_GPIO_NUM_DEFAULT;
-    const int wakeup_level = BUTTON_WAKEUP_LEVEL_DEFAULT;
-    gpio_config_t config = {
-            .pin_bit_mask = BIT64(button_gpio_num),
-            .mode = GPIO_MODE_INPUT
-    };
-    ESP_ERROR_CHECK(gpio_config(&config));
-    gpio_wakeup_enable(button_gpio_num,
-            wakeup_level == 0 ? GPIO_INTR_LOW_LEVEL : GPIO_INTR_HIGH_LEVEL);
+    
 
-
-    adc_init();
-    gpio_set_direction(GPIO_NUM_23, GPIO_MODE_OUTPUT);
-    nvs_flash_init();
-
-
+    //start wifi
     wifi_connection();
     vTaskDelay(2000 / portTICK_PERIOD_MS);
     printf("WIFI was initiated ...........\n");
 
-    mqtt_app_start();
-
-    vTaskDelay( 1000 / portTICK_RATE_MS );
-	xTaskCreate( &DHT_task, "DHT_task", 2048, NULL, 5, NULL );
-   
-    ///////
-    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SERVO_PULSE_GPIO); // To drive a RC servo, one MCPWM generator is enough
-
-
-    mcpwm_config_t pwm_config = {
-        .frequency = 50, // frequency = 50Hz, i.e. for every servo motor time period should be 20ms
-        .cmpr_a = 0,     // duty cycle of PWMxA = 0
-        .counter_mode = MCPWM_UP_COUNTER,
-        .duty_mode = MCPWM_DUTY_MODE_0,
-    };
-    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
-    /////////////
-
+    //variables
     esp_err_t ret = ESP_OK;
     uint32_t voltage0 = 0;
     uint32_t voltage1=0;
@@ -346,36 +295,68 @@ void app_main(void)
     uint32_t counter=0;
     bool pos=0;
 
+    //sleep configuration
+    const int button_gpio_num = BUTTON_GPIO_NUM_DEFAULT;
+    const int wakeup_level = BUTTON_WAKEUP_LEVEL_DEFAULT;
+    gpio_config_t config = {
+            .pin_bit_mask = BIT64(button_gpio_num),
+            .mode = GPIO_MODE_INPUT
+    };
+    ESP_ERROR_CHECK(gpio_config(&config));
+    gpio_wakeup_enable(button_gpio_num,
+            wakeup_level == 0 ? GPIO_INTR_LOW_LEVEL : GPIO_INTR_HIGH_LEVEL);
 
+
+    //start ADC
+    adc_init();
+    gpio_set_direction(GPIO_NUM_23, GPIO_MODE_OUTPUT);
+    nvs_flash_init();
+
+    
+
+    //start MQTT
+    mqtt_app_start();
+
+    //start DHT
+    vTaskDelay( 1000 / portTICK_RATE_MS );
+	xTaskCreate( &DHT_task, "DHT_task", 2048, NULL, 5, NULL );
+   
+    
+
+    //configure PWM settings
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SERVO_PULSE_GPIO); // To drive a RC servo, one MCPWM generator is enough
+    mcpwm_config_t pwm_config = {
+        .frequency = 50, // frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+        .cmpr_a = 0,     // duty cycle of PWMxA = 0
+        .counter_mode = MCPWM_UP_COUNTER,
+        .duty_mode = MCPWM_DUTY_MODE_0,
+    };
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+    
+
+    
     while (1) {
         
-        adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0); //moisture sensor to control the pump
+        adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0); //moisture sensor 1
         ESP_LOGI(TAG_CH[0][0], "raw  data: %d", adc_raw[0][0]);
-        
-        //{pump_in_state=1;gpio_set_level(GPIO_NUM_21, 1); printf("pump on");}
-        //else
-        //{pump_in_state=0;gpio_set_level(pump_gpio, pump_in_state);printf("pump off");}
 
         if (cali_enable) {
             voltage0 = esp_adc_cal_raw_to_voltage(adc_raw[0][0], &adc1_chars);
             ESP_LOGI(TAG_CH[0][0], "cali data: %d mV", voltage0);
             sprintf(str0,"%d",voltage0);
             esp_mqtt_client_publish(client,"ESP32/value", str0, 0, 1, 0);
-            
             esp_mqtt_client_publish(client,"ESP32/sensorID", "0", 0, 1, 0);
-
         }
         vTaskDelay(pdMS_TO_TICKS(800));
 
 
-        adc_raw[1][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN1);
+        adc_raw[1][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN1); //moisture sensor 2
         ESP_LOGI(TAG_CH[1][0], "raw  data: %d", adc_raw[1][0]);
         if (cali_enable) {
             voltage1 = esp_adc_cal_raw_to_voltage(adc_raw[1][0], &adc1_1_chars);
             ESP_LOGI(TAG_CH[1][0], "cali data: %d mV", voltage1);
             sprintf(str1,"%d",voltage1);
             esp_mqtt_client_publish(client,"ESP32/value", str1, 0, 1, 0);
-            
             esp_mqtt_client_publish(client,"ESP32/sensorID", "1", 0, 1, 0);
         }
         vTaskDelay(pdMS_TO_TICKS(800));
@@ -387,7 +368,6 @@ void app_main(void)
             ESP_LOGI(TAG_CH[2][0], "cali data: %d mV", voltage2);
             sprintf(str2,"%d",voltage2);
             esp_mqtt_client_publish(client,"ESP32/value",str2, 0, 1, 0);
-            
             esp_mqtt_client_publish(client,"ESP32/sensorID", "2", 0, 1, 0);
         }
         vTaskDelay(pdMS_TO_TICKS(800));
@@ -399,12 +379,12 @@ void app_main(void)
             ESP_LOGI(TAG_CH[3][0], "cali data: %d mV", voltage3);
             sprintf(str3,"%d",voltage3);
             esp_mqtt_client_publish(client, "ESP32/value",str3, 0, 1, 0);
-            
             esp_mqtt_client_publish(client,"ESP32/sensorID", "3", 0, 1, 0);
         }
         vTaskDelay(pdMS_TO_TICKS(800));
-        /////SERVO
         
+        
+        //SERVO turn 
         int angle;
 
         if(pos==0){
@@ -430,11 +410,36 @@ void app_main(void)
             vTaskDelay(pdMS_TO_TICKS(600)); 
             }
         }
-        /////check if water reservoir is still full enough 
+        //check water levels
+        if(voltage0<1000){//soil moisture
+            gpio_set_level(GPIO_NUM_21, 1); printf("pump on \n");
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            gpio_set_level(pump_gpio, pump_in_state);printf("pump off \n");
+        }
+
+        if(voltage1<1000){
+            esp_mqtt_client_publish(client, "ESP32/tank", "water lvl low! \n", 0, 1, 0);
+        }
 
 
-        ////
-        /////////////////light_sleep mode
+        //update LED status
+        if(voltage0<1000 && voltage1<1000 ){
+            gpio_set_level(GPIO_NUM_33, 1);
+            gpio_set_level(GPIO_NUM_15, 1);
+            gpio_set_level(GPIO_NUM_32, 1);
+        } else if(voltage0<1000 && voltage1<1000 ){
+            gpio_set_level(GPIO_NUM_33, 1);
+            gpio_set_level(GPIO_NUM_15, 1);
+            gpio_set_level(GPIO_NUM_32, 0);
+        } else {
+            gpio_set_level(GPIO_NUM_33, 1);
+            gpio_set_level(GPIO_NUM_15, 0);
+            gpio_set_level(GPIO_NUM_32, 0);
+        }
+
+
+
+        //light_sleep mode
         /* Wake up in 2 seconds, or when button is pressed */
         esp_sleep_enable_timer_wakeup(6000000);
         esp_sleep_enable_gpio_wakeup();
