6cee9e06a33892553d1e5a93a719436ba7764b0e
tayen.vankeer <tayen.vankeer@student.kuleuven.be>
2022-05-16 14:58:37 +0200

    FREERTOS

--- main/main.c
+++ main/main.c
@@ -104,6 +104,18 @@ static esp_adc_cal_characteristics_t adc2_chars;
 static EventGroupHandle_t s_wifi_event_group;
 static const char *TAGG = "wifi station";
 static int s_retry_num = 0;
+//variables
+    esp_err_t ret = ESP_OK;
+    uint32_t voltage0 = 0;
+    uint32_t voltage1=0;
+    uint32_t voltage2=0;
+    uint32_t voltage3=0;
+    char str0[50];
+    char str1[50];
+    char str2[50];
+    char str3[50];
+    int angle=0;
+    bool pos=0;
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -366,7 +378,7 @@ void DHT_task(void *pvParameter)
         readDHT();
         
 
-        char temp[50];
+       char temp[50];
         char hum[50];
         sprintf( temp,"%.1f\n", getHumidity() );
         sprintf(hum, "%.1f\n", getTemperature() );
@@ -398,137 +410,97 @@ static inline uint32_t example_convert_servo_angle_to_duty_us(int angle)
     return (angle + SERVO_MAX_DEGREE) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) / (2 * SERVO_MAX_DEGREE) + SERVO_MIN_PULSEWIDTH_US;
 }
 
-
-void app_main(void)
-{   
-    //variables
-    esp_err_t ret = ESP_OK;
-    uint32_t voltage0 = 0;
-    uint32_t voltage1=0;
-    uint32_t voltage2=0;
-    uint32_t voltage3=0;
-    bool cali_enable = adc_calibration_init();
-    char str0[50];
-    char str1[50];
-    char str2[50];
-    char str3[50];
-    int angle=0;
-    bool pos=0;
-    
-
-    //Initialize NVS
-    esp_err_t retu = nvs_flash_init();
-    if (retu == ESP_ERR_NVS_NO_FREE_PAGES || retu == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      retu = nvs_flash_init();
+void Task_moisture1(void *pvParameters)  // This is a task.
+{
+  printf( "Starting moisture_1 Task\n\n");
+  bool cali_enable = adc_calibration_init();
+  for (;;) // A Task shall never return or exit.
+  {
+    adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0); //moisture sensor 1
+    ESP_LOGI(TAG_CH[0][0], "raw  data: %d", adc_raw[0][0]);
+    if (cali_enable) 
+    {
+        voltage0 = esp_adc_cal_raw_to_voltage(adc_raw[0][0], &adc1_chars);
+        ESP_LOGI(TAG_CH[0][0], "cali data: %d mV", voltage0);
+        sprintf(str0,"%d",voltage0);
+        esp_mqtt_client_publish(client,"ESP32/value", str0, 0, 1, 0);
+        esp_mqtt_client_publish(client,"ESP32/sensorID", "0", 0, 1, 0);
     }
-    ESP_ERROR_CHECK(retu);
+    //vTaskDelay( 200000 / portTICK_PERIOD_MS ); // wait for one second
+    vTaskDelay(pdMS_TO_TICKS(5000));
+  }
+}
 
-    ESP_LOGI(TAGG, "ESP_WIFI_MODE_STA");
-    wifi_init_sta();
-
-
-    //sleep configuration
-    const int button_gpio_num = BUTTON_GPIO_NUM_DEFAULT;
-    const int wakeup_level = BUTTON_WAKEUP_LEVEL_DEFAULT;
-    gpio_config_t config = {
-            .pin_bit_mask = BIT64(button_gpio_num),
-            .mode = GPIO_MODE_INPUT
-    };
-    ESP_ERROR_CHECK(gpio_config(&config));
-    gpio_wakeup_enable(button_gpio_num,
-            wakeup_level == 0 ? GPIO_INTR_LOW_LEVEL : GPIO_INTR_HIGH_LEVEL);
-
-
-    //start ADC
-    adc_init();
-    gpio_set_direction(GPIO_NUM_23, GPIO_MODE_OUTPUT);
-    nvs_flash_init();
-
-    
-
-    //start MQTT
-    mqtt_app_start();
-
-    //start DHT
-    vTaskDelay( 1000 / portTICK_RATE_MS );
-	xTaskCreate( &DHT_task, "DHT_task", 2048, NULL, 5, NULL );
-   
-    
-
-    //configure PWM settings
-    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SERVO_PULSE_GPIO); // To drive a RC servo, one MCPWM generator is enough
-    mcpwm_config_t pwm_config = {
-        .frequency = 50, // frequency = 50Hz, i.e. for every servo motor time period should be 20ms
-        .cmpr_a = 0,     // duty cycle of PWMxA = 0
-        .counter_mode = MCPWM_UP_COUNTER,
-        .duty_mode = MCPWM_DUTY_MODE_0,
-    };
-    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
-    
-
-    
-    while (1) {
-        
-        adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0); //moisture sensor 1
-        ESP_LOGI(TAG_CH[0][0], "raw  data: %d", adc_raw[0][0]);
-
-        if (cali_enable) {
-            voltage0 = esp_adc_cal_raw_to_voltage(adc_raw[0][0], &adc1_chars);
-            ESP_LOGI(TAG_CH[0][0], "cali data: %d mV", voltage0);
-            sprintf(str0,"%d",voltage0);
-            esp_mqtt_client_publish(client,"ESP32/value", str0, 0, 1, 0);
-            esp_mqtt_client_publish(client,"ESP32/sensorID", "2", 0, 1, 0);
-        }
-        vTaskDelay(pdMS_TO_TICKS(800));
-
-
-        adc_raw[1][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN1); //moisture sensor 2
-        ESP_LOGI(TAG_CH[1][0], "raw  data: %d", adc_raw[1][0]);
-        if (cali_enable) {
+void Task_moisture2(void *pvParameters)  // This is a task.
+{ bool cali_enable = adc_calibration_init();
+  printf( "Starting moisture_2 Task\n\n");
+  for (;;) // A Task shall never return or exit.
+  {
+    adc_raw[1][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN1); //moisture sensor 2
+    ESP_LOGI(TAG_CH[1][0], "raw  data: %d", adc_raw[1][0]);
+    if (cali_enable) {
             voltage1 = esp_adc_cal_raw_to_voltage(adc_raw[1][0], &adc1_1_chars);
             ESP_LOGI(TAG_CH[1][0], "cali data: %d mV", voltage1);
             sprintf(str1,"%d",voltage1);
             esp_mqtt_client_publish(client,"ESP32/value", str1, 0, 1, 0);
-            esp_mqtt_client_publish(client,"ESP32/sensorID", "6", 0, 1, 0);
+            esp_mqtt_client_publish(client,"ESP32/sensorID", "1", 0, 1, 0);
         }
-        vTaskDelay(pdMS_TO_TICKS(800));
+    vTaskDelay(pdMS_TO_TICKS(5000));
+  }
+}
 
-        adc_raw[2][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN2);//light sensor1
+void Task_light1(void *pvParameters)  // This is a task.
+{ bool cali_enable = adc_calibration_init();
+  printf( "Starting light_1 Task\n\n");
+  for (;;) // A Task shall never return or exit.
+  {
+    adc_raw[2][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN2);//light sensor1
         ESP_LOGI(TAG_CH[2][0], "raw  data: %d", adc_raw[2][0]);
         if (cali_enable) {
             voltage2 = esp_adc_cal_raw_to_voltage(adc_raw[2][0], &adc1_2_chars);
             ESP_LOGI(TAG_CH[2][0], "cali data: %d mV", voltage2);
             sprintf(str2,"%d",voltage2);
             esp_mqtt_client_publish(client,"ESP32/value",str2, 0, 1, 0);
-            esp_mqtt_client_publish(client,"ESP32/sensorID", "4", 0, 1, 0);
+            esp_mqtt_client_publish(client,"ESP32/sensorID", "2", 0, 1, 0);
         }
-        vTaskDelay(pdMS_TO_TICKS(800));
+    vTaskDelay(pdMS_TO_TICKS(5000));
+  }
+}
 
-        adc_raw[3][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN3);//light sensor2
+void Task_light2(void *pvParameters)  // This is a task.
+{ bool cali_enable = adc_calibration_init();
+  printf( "Starting light_2 Task\n\n");
+  for (;;) // A Task shall never return or exit.
+  {
+    adc_raw[3][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN3);//light sensor2
         ESP_LOGI(TAG_CH[3][0], "raw  data: %d", adc_raw[3][0]);
         if (cali_enable) {
             voltage3 = esp_adc_cal_raw_to_voltage(adc_raw[3][0], &adc1_3_chars);
             ESP_LOGI(TAG_CH[3][0], "cali data: %d mV", voltage3);
             sprintf(str3,"%d",voltage3);
             esp_mqtt_client_publish(client, "ESP32/value",str3, 0, 1, 0);
-            esp_mqtt_client_publish(client,"ESP32/sensorID", "5", 0, 1, 0);
+            esp_mqtt_client_publish(client,"ESP32/sensorID", "3", 0, 1, 0);
         }
-        vTaskDelay(pdMS_TO_TICKS(800));
-        
-        
-        //SERVO turn 
-        if(rotate==1){
-            angle= angle + rotateAngle;
-            ESP_LOGI(TAG_servo, "Angle of rotation: %d", angle);
-            ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, example_convert_servo_angle_to_duty_us(angle)));
-            rotate=0;
-            vTaskDelay(pdMS_TO_TICKS(100)); 
-        }
+    vTaskDelay(pdMS_TO_TICKS(5000));
+  }
+}
 
+void Task_servo(void *pvParameters)  // This is a task.
+{
+  printf( "Starting servo Task\n\n");
+  for (;;) // A Task shall never return or exit.
+  {
+    //SERVO turn 
+    if(rotate==1){
+        angle= angle + rotateAngle;
+        ESP_LOGI(TAG_servo, "Angle of rotation: %d", angle);
+        ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, example_convert_servo_angle_to_duty_us(angle)));
+        rotate=0;
+        vTaskDelay(pdMS_TO_TICKS(100)); 
+    }  
 
-
-        if(pos==0){
+    if(pos==0)
+    {
         counter=counter+voltage2+voltage3;
         if(counter>10000){
             printf("threshold light reached 500V in total \n");
@@ -539,7 +511,9 @@ void app_main(void)
             ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, example_convert_servo_angle_to_duty_us(angle)));
             vTaskDelay(pdMS_TO_TICKS(600)); 
         }
-        } else if(pos==1){
+    }
+    else if(pos==1)
+    {
         counter=counter+voltage2+voltage3;
         if(counter>10000){
             printf("threshold light reached 500V in total \n");
@@ -550,33 +524,44 @@ void app_main(void)
             ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, example_convert_servo_angle_to_duty_us(angle)));
             vTaskDelay(pdMS_TO_TICKS(600)); 
             }
-        }
+    }
+    printf("counter value %d \n",counter);
+    vTaskDelay(pdMS_TO_TICKS(10000));
+  }
+}
 
-        printf("counter value %d \n",counter);
+void Task_waterLevel(void *pvParameters)  // This is a task.
+{
+  printf( "Starting check_water_level Task\n\n");
+  for (;;) // A Task shall never return or exit.
+  {
+   //check water levels
+    if(voltage0<thres_moist){//soil moisture
+        gpio_set_level(GPIO_NUM_21, 1); printf("pump on \n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        gpio_set_level(pump_gpio, pump_in_state);printf("pump off \n");
+        score--;
+    }else {
+        score++;
+    }
 
-    
+    if(voltage1<1000){//tank level
+        esp_mqtt_client_publish(client, "ESP32/tank", "water lvl low! \n", 0, 1, 0);
+        printf("water lvl low in tank low! \n");
+        score--;
+    } else{
+        score++;
+    }
+    vTaskDelay(pdMS_TO_TICKS(600)); 
+  }
+}
 
-        //check water levels
-        if(voltage0<thres_moist){//soil moisture
-            gpio_set_level(GPIO_NUM_21, 1); printf("pump on \n");
-            vTaskDelay(pdMS_TO_TICKS(1000));
-            gpio_set_level(pump_gpio, pump_in_state);printf("pump off \n");
-            score--;
-        }else {
-            score++;
-        }
-
-        if(voltage1<1000){//tank level
-            esp_mqtt_client_publish(client, "ESP32/tank", "water lvl low! \n", 0, 1, 0);
-            printf("water lvl low in tank low! \n");
-            score--;
-        } else{
-            score++;
-        }
-
-
-        //update LED status
-        if(score<=3){
+void Task_LED(void *pvParameters)  // This is a task.
+{
+  printf( "Starting update_LED_status Task\n\n");
+  for (;;) // A Task shall never return or exit.
+  {
+    if(score<=3 ){
             gpio_set_level(GPIO_NUM_33, 1);
             gpio_set_level(GPIO_NUM_15, 1);
             gpio_set_level(GPIO_NUM_32, 1);
@@ -589,18 +574,24 @@ void app_main(void)
             gpio_set_level(GPIO_NUM_15, 0);
             gpio_set_level(GPIO_NUM_32, 0);
         }
+    vTaskDelay(pdMS_TO_TICKS(600)); 
+  }
+}
 
-
+void Task_lightSleep(void *pvParameters)  // This is a task.
+{
+  printf( "Starting light_sleep_mode Task\n\n");
+  for (;;) // A Task shall never return or exit.
+  {
         esp_mqtt_client_disconnect(client);
         esp_mqtt_client_stop(client);
-        vTaskDelay(200);
+        vTaskDelay(500);
         esp_wifi_disconnect();
         esp_wifi_stop();
-        vTaskDelay(200);
+        vTaskDelay(500);
 
-        printf("sleepTime atm %d\n ",sleepTime);
         //light_sleep mode
-        // Wake up in 6 seconds
+        /* Wake up in 2 seconds, or when button is pressed */
         esp_sleep_enable_timer_wakeup(sleepTime*1000000);
         esp_sleep_enable_wifi_wakeup();
 
@@ -647,17 +638,73 @@ void app_main(void)
        
         esp_wifi_start();
         esp_wifi_connect();
-        vTaskDelay(500);
+        vTaskDelay(1000);
         esp_mqtt_client_start(client);
         esp_mqtt_client_reconnect(client);
-       
+        vTaskDelay(pdMS_TO_TICKS(600)); 
+  }
+}
 
-        
-        
-        
 
-        
-   
+
+void app_main(void)
+{   
+    bool cali_enable = adc_calibration_init();
+
+    //Initialize NVS
+    esp_err_t retu = nvs_flash_init();
+    if (retu == ESP_ERR_NVS_NO_FREE_PAGES || retu == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      retu = nvs_flash_init();
     }
+    ESP_ERROR_CHECK(retu);
+
+    ESP_LOGI(TAGG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+
+    
+
+    //sleep configuration
+    const int button_gpio_num = BUTTON_GPIO_NUM_DEFAULT;
+    const int wakeup_level = BUTTON_WAKEUP_LEVEL_DEFAULT;
+    gpio_config_t config = {
+            .pin_bit_mask = BIT64(button_gpio_num),
+            .mode = GPIO_MODE_INPUT
+    };
+    ESP_ERROR_CHECK(gpio_config(&config));
+    gpio_wakeup_enable(button_gpio_num,
+            wakeup_level == 0 ? GPIO_INTR_LOW_LEVEL : GPIO_INTR_HIGH_LEVEL);
+
+
+    //start ADC
+    adc_init();
+    gpio_set_direction(GPIO_NUM_23, GPIO_MODE_OUTPUT);
+    nvs_flash_init();
+
+    
+
+    //start MQTT
+    mqtt_app_start();
+
+    //configure PWM settings
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SERVO_PULSE_GPIO); // To drive a RC servo, one MCPWM generator is enough
+    mcpwm_config_t pwm_config = {
+        .frequency = 50, // frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+        .cmpr_a = 0,     // duty cycle of PWMxA = 0
+        .counter_mode = MCPWM_UP_COUNTER,
+        .duty_mode = MCPWM_DUTY_MODE_0,
+    };
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+
+    vTaskDelay( 1000 / portTICK_RATE_MS );
+	xTaskCreate( &DHT_task, "DHT_task", 2048, NULL, 5, NULL );
+    xTaskCreate(&Task_moisture1, "Task_moisture1",2048,NULL,5,NULL);
+    xTaskCreate(&Task_moisture2, "Task_moisture2",2048,NULL,5,NULL);
+    xTaskCreate(&Task_light1, "Task_light1",2048,NULL,5,NULL);
+    xTaskCreate(&Task_light2, "Task_light2",2048,NULL,5,NULL);
+    xTaskCreate(&Task_servo,"Task_servo",2048,NULL,5,NULL);
+    xTaskCreate(&Task_waterLevel,"Task_waterLevel",2048,NULL,5,NULL);
+    xTaskCreate(&Task_LED, "Task_LED",2048,NULL,5,NULL);
+    xTaskCreate(&Task_lightSleep,"Task_lightSleep",2048,NULL,5,NULL);
         
 }
